1e9a267d60286b0b16766927e44d854bbe3180b8
Sajid <saajiidi@gmail.com>
2026-02-16 10:13:32 +0600

    .

--- Extra/check_values.py
+++
@@ -1,5 +0,0 @@
-import pandas as pd
-
-fp = 'h:/Analysis/New_/orders-2026-02-14-16-09-28.xlsx'
-df = pd.read_excel(fp)
-print(df[['Item Name', 'Quantity', 'Item Cost']].head())
--- Extra/inspect_order.py
+++
@@ -1,5 +0,0 @@
-import pandas as pd
-
-df = pd.read_excel('Test_beta.xlsx')
-row = df[df['Order Number'] == 191280]
-print(row[['Order Number', 'Payment Method Title', 'trxId']])
